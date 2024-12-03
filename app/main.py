8d86ea8b6be7dbead48b69f44d65fa8fbc15874d
Koushik Romel <koushikromel@gmail.com>
2024-12-03 08:07:57 +0530

    Added timestamp

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 
 app = FastAPI()
@@ -6,4 +7,4 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return "Working"
+    return {"timestamp": datetime.now()}
